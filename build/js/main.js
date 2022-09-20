d763e4314254efada96578c371d784b8ac840994
Dmitriy Nekludov <nekludov.d.a@gmail.com>
2022-09-20 07:59:24 +0300

    Install and configure webpack

---
+++ build/js/main.js
@@ -0,0 +1,9 @@
+/******/ (function() { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!********************!*\
+  !*** ./js/main.js ***!
+  \********************/
+console.log("hi");
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
